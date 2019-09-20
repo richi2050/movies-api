40322f713b0203eb0f7ca61efbe058c76012a936
richi2050 <ricardo.lugo.recillas@gmail.com>
2019-09-19 21:05:38 -0500

    se creo las routes de movies con listado de peliculas

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ const express = require('express');
 const app = express();
 const { config } = require('./config/index');
 
-app.get('/', function(req, res){
-	res.send('Hello world');
-});
-
-app.get('/json',function(req, res){
-	res.json({hola : 'mundo'});
-});
+const moviesApi = require('./routes/movies.js');
+moviesApi(app);
 
 app.listen(config.port,function(){
 	console.log(`Listening http://localhost:${config.port}`);	
